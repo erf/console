6cc8110db912a26a8791fe15bfb05f132f2d9d44
Erlend Fagerheim <erlendf80@gmail.com>
2020-04-05 19:11:21 +0200

    added sta api

--- console.dart
+++ console.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 class Console {
+  final buffer = StringBuffer();
+
   Console() {}
 
-  set rawMode(bool rawMode) {
+  void set rawMode(bool rawMode) {
     if (rawMode) {
       stdin.echoMode = false;
       stdin.lineMode = false;
@@ -21,7 +23,7 @@ class Console {
     return stdout.terminalLines;
   }
 
-  void setColor(int color) {
+  void set setColor(int color) {
     stdout.write("\x1b[38;5;${color}m");
   }
 
@@ -29,21 +31,62 @@ class Console {
     stdout.write(object);
   }
 
-  Stream inputStream() {
+  Stream get input {
     return stdin.asBroadcastStream();
   }
+
+  void append(String str) {
+    buffer.write(str);
+  }
+
+  void apply() {
+    stdout.write(buffer);
+  }
+
+  void move(int row, int col) {
+    append("\x1b[${row};${col}H");
+  }
+
+  void cursor(bool visible) {
+    if (visible)
+      append("\x1b[?25h");
+    else
+      append("\x1b[?25l");
+  }
+
+  void clear() {
+    append("\x1b[H"); // Go home
+    append("\x1b[J"); // erase down
+  }
+
+  void color_fg(int color) {
+    append("\x1b[38;5;${color}m");
+  }
+
+  void color_bg(int color) {
+    append("\x1b[48;5;${color}m");
+  }
+
+  void color_reset() {
+    append("\x1b[0m");
+  }
+
+// TODO resize callback using SIGWINCH and ffi
+//void on_resize(resize_handler handler) {
+//signal(SIGWINCH, handler);
+//}
 }
 
 void main() {
-  final console = Console();
-  console.rawMode = true;
+  final c = Console();
+  c.rawMode = true;
 
-  console.inputStream().listen((codes) {
+  c.input.listen((codes) {
     String string = String.fromCharCodes(codes);
     stdout.write(string);
     stdout.write(codes);
   });
 
-  console.setColor(6);
-  console.write("[${console.cols}, ${console.rows}]");
+  c.setColor = 6;
+  c.write("[${c.cols}, ${c.rows}]");
 }
