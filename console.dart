6490137fb62baa52ccc79ef65bd6086658e60dbb
Erlend Fagerheim <erlendf80@gmail.com>
2020-04-05 18:30:33 +0200

    color

--- console.dart
+++ console.dart
@@ -1,12 +1,20 @@
 import 'dart:io';
 
+class Console {
+  Console() {
+  }
+}
+
 void main() {
   stdin.echoMode = false;
   stdin.lineMode = false;
 
-  stdin.listen((text) {
-    stdout.write(text);
+  stdin.listen((codes) {
+    String string = String.fromCharCodes(codes);
+    stdout.write(string);
+    stdout.write(codes);
   });
 
+  stdout.write("\x1b[38;5;${1}m");
   stdout.write("hello world");
 }
