4b712cdc95393d5cc85f363afc46cb0128c68ee7
Erlend Fagerheim <erlendf80@gmail.com>
2020-04-06 02:49:46 +0200

    single quotes

--- test/console_test.dart
+++ test/console_test.dart
@@ -11,38 +11,38 @@ void main() {
 
     test('move', () {
       console.move(row: 1, col: 2);
-      expect(console.buffer.toString(), "\x1b[1;2H");
+      expect(console.buffer.toString(), '\x1b[1;2H');
     });
 
     test('hide cursor', () {
       console.cursor = false;
-      expect(console.buffer.toString(), "\x1b[?25l");
+      expect(console.buffer.toString(), '\x1b[?25l');
     });
 
     test('clear', () {
       console.clear();
-      expect(console.buffer.toString(), "\x1b[H\x1b[J");
+      expect(console.buffer.toString(), '\x1b[H\x1b[J');
     });
 
     test('color_fg', () {
       console.color_fg = 1;
-      expect(console.buffer.toString(), "\x1b[38;5;1m");
+      expect(console.buffer.toString(), '\x1b[38;5;1m');
     });
 
     test('color_bg', () {
       console.color_bg = 6;
-      expect(console.buffer.toString(), "\x1b[48;5;6m");
+      expect(console.buffer.toString(), '\x1b[48;5;6m');
     });
 
     test('color_reset', () {
       console.color_reset();
-      expect(console.buffer.toString(), "\x1b[0m");
+      expect(console.buffer.toString(), '\x1b[0m');
     });
 
     test('two commands', () {
       console.color_fg = 1;
       console.append('hello');
-      expect(console.buffer.toString(), "\x1b[38;5;1mhello");
+      expect(console.buffer.toString(), '\x1b[38;5;1mhello');
     });
 
     test('clear buffer', () {
